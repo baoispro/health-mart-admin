234072efe187743597b3800857b261e2761556b3
Tran-Van-Luc <tranvanluc2506@gmail.com>
2025-04-28 04:24:31 +0700

    feat(reviews): add sorting, filtering, and CRU operations for reviews

--- app/routes.ts
+++ app/routes.ts
@@ -9,7 +9,8 @@ export default [
   layout("components/layout/MainLayout.tsx", [
     index("routes/home.tsx"),
     route("users", "routes/users.tsx"),
-    route("policies", "routes/policies.tsx")
+    route("policies", "routes/policies.tsx"),
+    route("reviews", "routes/reviews.tsx")
     // thêm route khác muốn dùng layout chung ở đây
   ]),
   route("sign-in", "routes/sign-in.tsx"),
